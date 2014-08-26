d0c44e0687b5099a2ed7fbb2e3c4f9d0f6e57114
Teemu Pajarinen <manakeri82@gmail.com>
2014-08-26 23:23:45 +0300

    tries with patterns

--- main.c
+++ main.c
@@ -1,19 +1,37 @@
+#define NO_ADDR_DEFINES 1
+
 #include <pic14/pic16f88.h>
 #include <stdint.h>
 
-__code uint16_t __at (_CONFIG1) cfg0 = 
+__code uint16_t __at (_CONFIG1) cfg0 =
  _INTRC_IO &
- _CP_OFF & 
- _WDT_OFF & 
- _BODEN_OFF & 
+ _CP_OFF &
+ _WDT_OFF &
+ _BODEN_OFF &
  _PWRTE_OFF &
- _MCLRE_ON & 
+ _MCLRE_ON &
  _DEBUG_OFF &
  _LVP_OFF;
 
-#define tstbit(var, bit) ((var) & (1 << (bit)))
-#define setbit(var, bit) ((var) |= (1 << (bit)))
-#define clrbit(var, bit) ((var) &= ~(1 << (bit)))
+#define SET_BIT(val, bitIndex)    ((val) |=  (1 << bitIndex) )
+#define CLEAR_BIT(val, bitIndex)  ((val) &= ~(1 << bitIndex) )
+#define TOGGLE_BIT(val, bitIndex) ((val) ^=  (1 << bitIndex) )
+#define BIT_IS_SET(val, bitIndex) ((val) &   (1 << bitIndex) )
+
+/* Port definitions */
+#define LAYER1 RA4
+#define LAYER2 RA3
+#define LAYER3 RA2
+
+#define LED1 RB1
+#define LED2 RB0
+#define LED3 RA6
+#define LED4 RB2
+#define LED5 RB6
+#define LED6 RB7
+#define LED7 RB3
+#define LED8 RB4
+#define LED9 RB5
 
 /* Adjust to your clock frequency (in Hz). */
 #define CLOCKFREQ   (8U*1000U*1000U)
@@ -33,84 +51,52 @@ void delay_ms(uint16_t ms) {
     }
 }
 
-#define LAYER1 RA4
-#define LAYER2 RA3
-#define LAYER3 RA2
-#define LED1 RB1
-#define LED2 RB0
-#define LED3 RA6
-#define LED4 RB2
-#define LED5 RB6
-#define LED6 RB7
-#define LED7 RB3
-#define LED8 RB4
-#define LED9 RB5
-
-struct pattern {
-	unsigned char f1:3,f2:3,f3:3;
-	unsigned char m1:3,m2:3,m3:3;
-	unsigned char b1:3,b2:3,b3:3;
+struct frame {
+	unsigned char f1 : 3;
+	unsigned char f2 : 3;
+        unsigned char f3 : 3;
+        unsigned char m1 : 3;
+        unsigned char m2 : 3;
+        unsigned char m3 : 3;
+        unsigned char b1 : 3;
+        unsigned char b2 : 3;
+        unsigned char b3 : 3;
+	unsigned char delay;
 };
 
-const struct pattern imageTab[] = {
- { 0b111, 0b111, 0b111, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000 }, 
- { 0b000, 0b000, 0b000, 0b111, 0b111, 0b111, 0b000, 0b000, 0b000 }, 
- { 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b111, 0b111, 0b111 }
+const struct frame pattern[] = {
+ { 0b111, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 1000 }, 
+ { 0b000, 0b000, 0b000, 0b000, 0b111, 0b000, 0b000, 0b000, 0b000, 1000 }, 
+ { 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b111, 1000 }, 
+ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 };
 
-void leds_on()
+void leds_on(unsigned char p, unsigned char layer)
 {
-    LED1 = 1;
-    LED2 = 1;
-    LED3 = 1;
-    LED4 = 1;
-    LED5 = 1;
-    LED6 = 1;
-    LED7 = 1;
-    LED8 = 1;
-    LED9 = 1;
+	LED1 = BIT_IS_SET(pattern[p].f1, layer);
+	LED2 = BIT_IS_SET(pattern[p].f2, layer);
+	LED3 = BIT_IS_SET(pattern[p].f3, layer);
+	LED4 = BIT_IS_SET(pattern[p].m1, layer);
+	LED5 = BIT_IS_SET(pattern[p].m2, layer);
+	LED6 = BIT_IS_SET(pattern[p].m3, layer);
+	LED7 = BIT_IS_SET(pattern[p].b1, layer);
+	LED8 = BIT_IS_SET(pattern[p].b2, layer);
+	LED9 = BIT_IS_SET(pattern[p].b3, layer);
 }
-
-void leds_off()
-{
-    LED1 = 0;
-    LED2 = 0;
-    LED3 = 0;
-    LED4 = 0;
-    LED5 = 0;
-    LED6 = 0;
-    LED7 = 0;
-    LED8 = 0;
-    LED9 = 0;
-}
-
 void main(void)
 {
-    unsigned char l;
-    INTCON = 0;	// no interrupts
-    ANSEL = 0x00; // no analog input
-    TRISA = 0b00000000; // all output
-    TRISB = 0b00000000; // all output
-    PORTA = 0x00;
-
-    leds_on();
-
-    for (;;) {
-        LAYER1 = 1;
-        delay_ms(1000);
-	LAYER1 = 0;
-        delay_ms(1000);
-
-        LAYER2 = 1;
-        delay_ms(1000);
-	LAYER2 = 0;
-        delay_ms(1000);
-
-        LAYER3 = 1;
-        delay_ms(1000);
-	LAYER3 = 0;
-        delay_ms(1000);
-
-    }
-
+	unsigned char f = 0;
+	unsigned char l = 0;
+	INTCON = 0;
+	TRISA = 0b00000000;
+	TRISB = 0b00000000;
+	for (;;) {
+		for (l=0;l<3;l++) {
+			leds_on(f,l);
+			delay_ms( pattern[f].delay );
+		}
+		f++;
+		if ( pattern[f].delay==0 ) 
+			f = 0;
+    	}
 }
