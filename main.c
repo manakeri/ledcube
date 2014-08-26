547e82f77ad72eb40f496de93c774802d0f16134
Teemu Pajarinen <manakeri82@gmail.com>
2014-08-26 15:21:27 +0300

    PATTERN TRIES

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ __code uint16_t __at (_CONFIG1) cfg0 =
  _DEBUG_OFF &
  _LVP_OFF;
 
-// bit macros
 #define tstbit(var, bit) ((var) & (1 << (bit)))
 #define setbit(var, bit) ((var) |= (1 << (bit)))
 #define clrbit(var, bit) ((var) &= ~(1 << (bit)))
@@ -33,23 +32,85 @@ void delay_ms(uint16_t ms) {
         }
     }
 }
+
+#define LAYER1 RA4
+#define LAYER2 RA3
+#define LAYER3 RA2
+#define LED1 RB1
+#define LED2 RB0
+#define LED3 RA6
+#define LED4 RB2
+#define LED5 RB6
+#define LED6 RB7
+#define LED7 RB3
+#define LED8 RB4
+#define LED9 RB5
+
+struct pattern {
+	unsigned char f1:3,f2:3,f3:3;
+	unsigned char m1:3,m2:3,m3:3;
+	unsigned char b1:3,b2:3,b3:3;
+};
+
+const struct pattern imageTab[] = {
+ { 0b111, 0b111, 0b111, 0b000, 0b000, 0b000, 0b000, 0b000, 0b000 }, 
+ { 0b000, 0b000, 0b000, 0b111, 0b111, 0b111, 0b000, 0b000, 0b000 }, 
+ { 0b000, 0b000, 0b000, 0b000, 0b000, 0b000, 0b111, 0b111, 0b111 }
+};
+
+void leds_on()
+{
+    LED1 = 1;
+    LED2 = 1;
+    LED3 = 1;
+    LED4 = 1;
+    LED5 = 1;
+    LED6 = 1;
+    LED7 = 1;
+    LED8 = 1;
+    LED9 = 1;
+}
+
+void leds_off()
+{
+    LED1 = 0;
+    LED2 = 0;
+    LED3 = 0;
+    LED4 = 0;
+    LED5 = 0;
+    LED6 = 0;
+    LED7 = 0;
+    LED8 = 0;
+    LED9 = 0;
+}
+
 void main(void)
 {
+    unsigned char l;
     INTCON = 0;	// no interrupts
     ANSEL = 0x00; // no analog input
-
     TRISA = 0b00000000; // all output
     TRISB = 0b00000000; // all output
+    PORTA = 0x00;
+
+    leds_on();
 
     for (;;) {
-//        setbit(LATA, 5);
-        PORTA = 0xFF;
-        PORTB = 0xFF;
-        delay_ms(100);
-//        clrbit(LATB, 5);
-        PORTA = 0x00;
-        PORTB = 0x00;
-        delay_ms(100);
+        LAYER1 = 1;
+        delay_ms(1000);
+	LAYER1 = 0;
+        delay_ms(1000);
+
+        LAYER2 = 1;
+        delay_ms(1000);
+	LAYER2 = 0;
+        delay_ms(1000);
+
+        LAYER3 = 1;
+        delay_ms(1000);
+	LAYER3 = 0;
+        delay_ms(1000);
+
     }
 
 }
